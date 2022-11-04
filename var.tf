18b66e94b8e7d637a1311b870f3bcef810cdee81
dibango <dibangoxx@gmail.com>
2022-11-03 19:54:46 -0500

    added ec2

--- var.tf
+++ var.tf
@@ -8,7 +8,7 @@ variable "AMI_ID" {
 variable "instance_type" {
     type = string
     description = "type of EC2 instance to be deployed"
-    default = "t3.medium"
+    default = "t3.micro"
   
 }
 
